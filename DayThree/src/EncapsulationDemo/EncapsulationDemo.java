5eaeb407d74b04a74021a1a2d2b0e2a7472846a2
Rishikesh616 <tpagare951@gmail.com>
2025-07-04 10:29:27 +0530

    This Day has Encapsulation, Scanner, and constructor concepts.

--- DayThree/src/EncapsulationDemo/EncapsulationDemo.java
+++ DayThree/src/EncapsulationDemo/EncapsulationDemo.java
@@ -1,5 +1,15 @@
 package EncapsulationDemo;
 
 public class EncapsulationDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+		OopsConceptDemo obj=new OopsConceptDemo();
+		
+		obj.setName("Rishikesh");
+		obj.setAge(21);
+		obj.setSerailNum(1001);
+		
+		System.out.println(obj);
+	}
 }
