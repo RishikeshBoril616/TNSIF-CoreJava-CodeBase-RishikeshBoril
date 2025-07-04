362b93b7cef6187221505ea2884fc80957985021
Rishikesh616 <tpagare951@gmail.com>
2025-07-04 12:08:32 +0530

    day6

---
+++ Day6/src/com/TNSIF/Day6/staticMethod/EmployeeDemo.java
@@ -0,0 +1,13 @@
+package com.TNSIF.Day6.staticMethod;
+
+public class EmployeeDemo {
+public static void main(String[] args) {
+		
+		
+		//Creating first object of the class 
+		
+		Employee e=new Employee("Rishikesh", 1001);
+		System.out.println(e);
+
+	}
+}
---
+++ Day6/src/com/TNSIF/Day6/staticMethod/MyClassDemo.java
@@ -0,0 +1,26 @@
+package com.TNSIF.Day6.staticMethod;
+
+public class MyClassDemo {
+public static void main(String[] args) {
+		
+		MyClass.display();
+		
+		System.out.println();
+		
+		MyClass o1=new MyClass();
+		System.out.println(o1);
+		
+		MyClass.display();
+		
+		System.out.println();
+		
+		MyClass o2=new MyClass();
+		System.out.println(o2);
+
+		System.out.println();
+		
+		MyClass o3=new MyClass();
+		System.out.println(o3);
+		
+	}
+}
