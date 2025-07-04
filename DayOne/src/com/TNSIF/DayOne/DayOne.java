3c490daea7d1a310452d3b8336ae9ab0bd172dd0
Rishikesh616 <tpagare951@gmail.com>
2025-07-04 11:42:51 +0530

    This is Day One.. start of the TNS Java Journey.

---
+++ DayOne/src/com/TNSIF/DayOne/DayOne.java
@@ -0,0 +1,7 @@
+package com.TNSIF.DayOne;
+
+public class DayOne {
+ public static void main(String[] args) {
+	System.out.println("Well This is my First Java Program Ever! Pretty Late if you think.. But at least I'm Here!");
+}
+}
