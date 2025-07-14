efb69d303bbdbf2134579816ce8b3e4163df3684
Rishikesh616 <tpagare951@gmail.com>
2025-07-14 12:50:32 +0530

    This day involves lists, queue and deque.

---
+++ Day 14/src/com/TNSIF/Day14/listdemo/ExDequeDemo.java
@@ -0,0 +1,32 @@
+package com.TNSIF.Day14.listdemo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ExDequeDemo 
+{
+	public static void main(String[] args) 
+	{
+		Deque<String> deque1=new ArrayDeque<String>();
+		deque1.add("Rishikesh");
+		deque1.add("Gojo Satoru");
+		deque1.add("Yuji Itadori");
+		deque1.add("Nobara Kugisaki");
+		deque1.add("Megumi Fushiguro");
+		
+		System.out.println(deque1);
+		
+		deque1.pollLast();
+		System.out.println(deque1);
+		
+		deque1.pollFirst();
+		System.out.println(deque1);
+		
+		for(String str: deque1)
+		{
+			System.out.println("Queue is: "+ str);
+		}
+		
+		System.out.println(deque1);
+	}
+}
---
+++ Day 14/src/com/TNSIF/Day14/listdemo/ExQueue1.java
@@ -0,0 +1,43 @@
+package com.TNSIF.Day14.listdemo;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ExQueue1 
+{
+	public static void main(String[] args) 
+	{
+	Queue<Integer> r=new LinkedList<Integer>();
+	
+	r.add(6);
+	r.add(10);
+	r.add(12);
+	r.add(13);
+	r.add(2);
+	
+	System.out.println(r);
+	
+	int positionPeer=r.peek();
+	System.out.println("Using Peek Method First Value From the Queue: "+ r);
+	
+	int positionelements=r.element();
+	System.out.println("Using Peek Method First Value From the Queue: "+ r);
+	
+	System.out.println();
+	System.out.println(r.remove(r));
+	System.out.println(r);
+	
+	Iterator itr=r.iterator();
+	while(itr.hasNext())
+	{
+		System.out.println(itr.next()+"\t");
+	}
+	System.out.println();
+	
+	System.out.println(r.poll());
+	System.out.println(r);
+	
+	System.out.println(r.retainAll(r));
+	}
+}
